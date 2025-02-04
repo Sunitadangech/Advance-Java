05646b2cdcb756dc76860547509378dbe9e25e1c
Sunitadangech <133972866+Sunitadangech@users.noreply.github.com>
2025-02-04 23:15:18 +0530

    Update SpringbootApplication.java

--- SpringbootApplication.java
+++ SpringbootApplication.java
@@ -1,13 +1,1 @@
-package com.jspiders.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringbootApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
-
-}
